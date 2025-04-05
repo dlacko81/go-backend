b5c5271a350a39561b582c855e361352ad3a6d69
dlacko81 <lacko.daniel81@gmail.com>
2025-04-05 22:52:50 +0200

    Add files via upload

---
+++ models/GO.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose');
+
+const GOSchema = new mongoose.Schema({
+  name: String,
+  country: String,
+  volume: Number,
+  tech: String,
+  date: Date
+});
+
+module.exports = mongoose.model('GO', GOSchema);
