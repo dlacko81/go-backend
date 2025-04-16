d31887f99ae88ce13410cc4c43b3da07dc2d2f06
dlacko81 <lacko.daniel81@gmail.com>
2025-04-16 16:21:42 +0200

    Update sheets.go

--- sheets/sheets.go
+++ sheets/sheets.go
@@ -3,29 +3,27 @@ package sheets
 import (
 	"context"
 	"log"
-	"google.golang.org/api/sheets/v4"
+
 	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
-// Function to get data from Google Sheets
-func GetSheetData() ([]interface{}, error) {
+func GetSheetData() ([][]interface{}, error) {
 	ctx := context.Background()
 
-	// Create a new Sheets API client
-	srv, err := sheets.NewService(ctx, option.WithAPIKey("YOUR_GOOGLE_API_KEY"))
+	// Assumes "credentials.json" is in the root of your Render project directory
+	srv, err := sheets.NewService(ctx, option.WithCredentialsFile("credentials.json"))
 	if err != nil {
-		log.Fatalf("Unable to create Sheets service: %v", err)
+		log.Printf("Unable to retrieve Sheets client: %v", err)
 		return nil, err
 	}
 
-	// Specify the spreadsheet ID and the range of cells
-	spreadsheetId := "https://docs.google.com/spreadsheets/d/1MEyhm03JvbMPC4PTn7-NUraqYx6KZx0SH5xffCjbC2A/edit?usp=sharing" // Replace with your actual spreadsheet ID
-	readRange := "Sheet1!A1:F10"           // Adjust the range as needed
+	spreadsheetId := "1MEyhm03JvbMPC4PTn7-NUraqYx6KZx0SH5xffCjbC2A"
+	readRange := "Sheet1" // Change this if your sheet has a different name
 
-	// Retrieve data from Google Sheets
 	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		log.Printf("Unable to retrieve data from sheet: %v", err)
 		return nil, err
 	}
 
