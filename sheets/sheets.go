8286cc07474f718de724d651f87dc4e555deb62a
dlacko81 <lacko.daniel81@gmail.com>
2025-04-16 17:18:24 +0200

    Update sheets.go

--- sheets/sheets.go
+++ sheets/sheets.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
+// Reads data from the Google Sheet
 func GetSheetData() ([][]interface{}, error) {
 	ctx := context.Background()
 
@@ -28,4 +29,36 @@ func GetSheetData() ([][]interface{}, error) {
 	}
 
 	return resp.Values, nil
+}
+
+// Appends a new row to the Google Sheet
+func AppendRow(row []interface{}) error {
+	ctx := context.Background()
+
+	// Load the Sheets service
+	srv, err := sheets.NewService(ctx, option.WithCredentialsFile("credentials.json"))
+	if err != nil {
+		log.Printf("Unable to retrieve Sheets client: %v", err)
+		return err
+	}
+
+	spreadsheetId := "1MEyhm03JvbMPC4PTn7-NUraqYx6KZx0SH5xffCjbC2A"
+	writeRange := "Sheet1" // Adjust if needed
+
+	valueRange := &sheets.ValueRange{
+		Values: [][]interface{}{row},
+	}
+
+	_, err = srv.Spreadsheets.Values.Append(spreadsheetId, writeRange, valueRange).
+		ValueInputOption("USER_ENTERED").
+		InsertDataOption("INSERT_ROWS").
+		Context(ctx).
+		Do()
+
+	if err != nil {
+		log.Printf("Unable to append data to sheet: %v", err)
+		return err
+	}
+
+	return nil
 }
